d4c65f20df25cc3884ee7a184ab01682d02913ac
Fred Blaise <fred.blaise@gmail.com>
2018-11-12 13:32:36 +0100

    more files

---
+++ dotfiles/i3/scripts/xrandr.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -x
+# First argument: Primary monitor
+# Second argument: Secondary monitor
+xrandr --auto
+
+if [[ -z $2 ]] && [[ -z $3 ]]; then
+	echo "laptop monitor only"
+	# Xft.dpi instead of scaling
+	xrandr --output "$1" --primary 
+
+elif xrandr | grep -q "$3 connected"; then
+	echo "home 3 monitors setup"
+	# rotation normal
+#	xrandr --output "$3" --mode 1920x1200 --pos 640x0 --rate 59.95 --output "$1" --scale 0.8x0.8 --pos 0x1200 --output "$2" --primary --mode 3840x2160 --pos 2560x0 --rate 60.00
+	xrandr --output "$3" --mode 1920x1200 --pos 640x0 --rate 59.95 --output "$1" --pos 0x1200 --output "$2" --primary --mode 3840x2160 --pos 2560x0 --rate 60.00
+	
+	# vertical rotation
+	# xrandr --output "$3" --rotate left --mode 1920x1200 --pos 1360x0 --rate 59.95 --output "$1" --scale 0.8x0.8 --pos 0x1920 --output "$2" --primary --mode 3840x2160 --pos 2560x0 --rate 60.00
+
+elif xrandr | grep -q "$2 connected"; then
+	echo "2 monitors setup"
+#	xrandr --output "$1" --scale 0.8x0.8 --output "$2" --primary --scale 1x1 --panning 2560x1440 --right-of "$1" 
+#	xrandr --output "$1" --scale 0.8x0.8 --pos 0x720 --output "$2" --primary --mode 3840x2160 --pos 2560x0 --rate 60.00
+	xrandr --output "$1" --pos 0x720 --output "$2" --primary --mode 3840x2160 --pos 3200x0 --rate 60.00
+	sleep 1
+	xrandr --output "${3%-*}" --off
+
+else
+	echo "Default: laptop monitor only"
+	xrandr --output "$1" --primary --output "$2" --off --output "$3" --off
+fi
